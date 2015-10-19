0d17aec4e311b1c40f0db413ef3e60a794d7d077
kyoheikato <k.kato@coins.tsukuba.ac.jp>
2015-10-19 12:21:54 +0900

    Fix private fields

--- mariscal/tweet.py
+++ mariscal/tweet.py
@@ -4,27 +4,27 @@ from pyramid.threadlocal import get_current_registry
 
 
 class TweetUtil:
-    _session = dict()
+    __session = dict()
 
     def __init__(self, user):
         settings = get_current_registry().settings
-        self._auth = tweepy.OAuthHandler(settings.get('consumer_key'), settings.get('consumer_secret'))
+        self.__auth = tweepy.OAuthHandler(settings.get('consumer_key'), settings.get('consumer_secret'))
         if user.is_twitter_account_exist():
-            self._auth.set_access_token(user.twitter_access_token, user.twitter_access_token_secret)
-            self._api = tweepy.API(self._auth)
+            self.__auth.set_access_token(user.twitter_access_token, user.twitter_access_token_secret)
+            self.__api = tweepy.API(self.__auth)
 
     def get_authenticate_url(self):
-        authenticate_url = self._auth.get_authorization_url()
-        TweetUtil._session['request_token'] = self._auth.request_token
+        authenticate_url = self.__auth.get_authorization_url()
+        TweetUtil.__session['request_token'] = self.__auth.request_token
 
         return authenticate_url
 
     def auth_twitter_access_token(self, pin_code, user):
-        self._auth.request_token = TweetUtil._session.get('request_token')
-        access_token = self._auth.get_access_token(verifier=pin_code)
+        self.__auth.request_token = TweetUtil.__session.get('request_token')
+        access_token = self.__auth.get_access_token(verifier=pin_code)
 
         if access_token:
-            self._auth.set_access_token(access_token[0], access_token[1])
+            self.__auth.set_access_token(access_token[0], access_token[1])
             user.set_twitter_access_token(access_token)
 
         # TODO: error handling
@@ -32,5 +32,5 @@ class TweetUtil:
             pass
 
     def get_tweets(self):
-        return self._api.user_timeline()
+        return self.__api.user_timeline()
 
